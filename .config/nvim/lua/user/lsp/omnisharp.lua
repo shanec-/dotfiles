4939f4111803b0c1f6e4e44e012c8585d13a7fc7
shane carvalho <shanec_@hotmail.com>
2023-01-01 23:00:15 +1100

    updated deprecated entries

--- .config/nvim/lua/user/lsp/omnisharp.lua
+++ .config/nvim/lua/user/lsp/omnisharp.lua
@@ -1,6 +1,6 @@
 -- omnisharp lsp config
 require'lspconfig'.omnisharp.setup {
-  capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+  capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities()),
   on_attach = function(_, bufnr)
     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
   end,
