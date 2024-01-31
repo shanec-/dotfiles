a7017365e7af8aad13bfb1232b68c4077ed3d1b4
shane carvalho <shanec_@hotmail.com>
2024-01-31 23:52:58 +1100

    added omnisharp specific config

--- .config/nvim/lua/user/lsp/omnisharp.lua
+++ .config/nvim/lua/user/lsp/omnisharp.lua
@@ -1,5 +1,14 @@
 -- omnisharp lsp config
 require'lspconfig'.omnisharp.setup {
+  on_attach = function (client, bufnr)
+    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+    -- buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+    local opts = { noremap = true, silent = true }
+
+    buf_set_keymap('n', '<leader>gd', ':lua require"omnisharp_extended".telescope_lsp_definitions()<CR>', opts)
+  end,
   handlers = {
     ["textDocument/definition"] = require('omnisharp_extended').handler,
   },
